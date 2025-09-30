c19cdca919d81d496a7c2982fc76b1ae8093a8a6
Edith Agai <email.02yt@gmail.com>
2025-09-30 11:10:29 +0300

    basic main structure to edit

---
+++ app/main.py
@@ -0,0 +1,33 @@
+#POST ADDING THE MODULES
+#import pygame
+#import sys
+#import random
+#from snake import Snake
+#from food import Food
+#from graphics import Graphics
+#from input_handler import InputHandler
+#from score import ScoreManager
+#from utils import *
+
+#snake = Snake()
+#food = Food()
+#graphics = Graphics(screen)
+#input_handler = InputHandler()
+#score = ScoreManager()
+
+#while running:
+    #direction = input_handler.get_direction(direction)
+    #snake.move(direction)
+
+   # if snake.head == food.position:
+       # snake.grow()
+       # score.add_point()
+       # food.spawn()
+
+   # if snake.collided():
+       # running = False
+
+   # graphics.draw_background()
+   # graphics.draw_snake(snake)
+   # graphics.draw_food(food)
+   # graphics.draw_score(score)
